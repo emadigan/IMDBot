533f38d9ff59cd031656fc967557f1815e8432c5
Emily Madigan <madigan.emily919@gmail.com>
2019-01-19 16:22:51 -0800

    Added fluff

--- IMDBot.py
+++ IMDBot.py
@@ -22,17 +22,6 @@ class LaunchRequestHandler(AbstractRequestHandler):
           False)
         return handler_input.response_builder.response
 
-class CreateTableIntentHandler(AbstractRequestHandler):
-    def can_handle(self, handler_input):
-        return is_intent_name("CreateTableIntent")(handler_input)
-        
-    def handle(self, handler_input):
-        speech_text = "DROP TABLE"
-            
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text)).set_should_end_session(True)
-        return handler_input.response_builder.response
-    
 class TopIMDBIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_intent_name("TopIMDBIntent")(handler_input)
@@ -42,27 +31,24 @@ class TopIMDBIntentHandler(AbstractRequestHandler):
         handler_input.response_builder.speak(speech_text).set_card(
             SimpleCard("Hello World", speech_text)).set_should_end_session(True)
         return handler_input.response_builder.response
-        
-    
-class HelloWorldIntentHandler(AbstractRequestHandler):
+
+class SearchIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
-        return is_intent_name("HelloWorldIntent")(handler_input)
+        return is_intent_name("SearchIntent")(handler_input)
 
     def handle(self, handler_input):
-        speech_text = "Hello World"
-
+        speech_text = top200.random_movie()
         handler_input.response_builder.speak(speech_text).set_card(
-          SimpleCard("Hello World", speech_text)).set_should_end_session(True)
+            SimpleCard("Hello World", speech_text)).set_should_end_session(True)
         return handler_input.response_builder.response
 
 class CancelAndStopIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
-        return is_intent_name("AMAZON.CancelIntent")(handler_input)\
+        return is_intent_name("AMAZON.CancelIntent")(handler_input)
         or is_intent_name("AMAZON.StopIntent")(handler_input)
 
     def handle(self, handler_input):
         return handler_input.response_builder.response
-
 
 class SessionEndedRequestHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
@@ -72,7 +58,6 @@ class SessionEndedRequestHandler(AbstractRequestHandler):
         return handler_input.response_builder.response
 
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
-
 
 class AllExceptionHandler(AbstractExceptionHandler):
     def can_handle(self, handler_input, exception):
@@ -87,11 +72,10 @@ class AllExceptionHandler(AbstractExceptionHandler):
 
 
 sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(HelloWorldIntentHandler())
 sb.add_request_handler(CancelAndStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
-sb.add_request_handler(CreateTableIntentHandler())
 sb.add_request_handler(TopIMDBIntentHandler())
 sb.add_exception_handler(AllExceptionHandler())
+sb.add_exception_handler(SearchIntentHandler())
 
 handler = sb.lambda_handler()
